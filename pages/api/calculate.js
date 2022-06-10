d995a1186229bc43bce94e60c4c4cf41a3a4b1b5
Burak Tokak <iletisim@buraktokak.com>
2022-06-10 17:55:25 +0300

    main flow and backend

--- pages/api/calculate.js
+++ pages/api/calculate.js
@@ -7,13 +7,14 @@ const handler = async (req, res) => {
   try {
 
     const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-    const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+    const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], onlyAudits: ['network-requests'], port: chrome.port};
     const runnerResult = await lighthouse(url, options);
 
     res.status(200).json({
       url: runnerResult.lhr.finalUrl,
       score: runnerResult.lhr.categories.performance.score * 100,
-      results: runnerResult.lhr.categories
+      results: runnerResult.lhr.categories,
+      audits: runnerResult.lhr.audits
     })
 
     await chrome.kill();
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,61 @@ import Header from '../components/Header'
 import post from '../js/post'
 import { useState, useEffect } from 'react'
 
+const CARBON_PER_KB = 0.00082;
+
 export default function Home() {
   const [url, setUrl] = useState("");
   const [calculating, setCalculating] = useState(false);
   const [results, setResults] = useState({});
+  const [pageSize, setPageSize] = useState({});
+  const [footPrint, setFootPrint] = useState({});
 
   let calculate = async () => {
     setCalculating(true);
     let calculation = await post("/api/calculate", {url})
-    console.log(calculation.results);
-    setResults(calculation.results);
+    console.log(calculation);
+
+    let pageSizeInKb = calculatePageSize(calculation.audits["network-requests"].details.items);
+    console.log(pageSizeInKb);
+    setPageSize(pageSizeInKb);
+    setResults(calculation);
+
+    let calculatedFootPrint = calculateCarbonFootPrint(pageSizeInKb, calculation.score);
+    setFootPrint(calculatedFootPrint);
+    console.log("footPrint", calculatedFootPrint);
     setCalculating(false);
+  }
+
+  let calculatePageSize = (items) => {
+    let totalInByte = 0;
+    let totalInByteWithoutStatic = 0;
+    items.forEach((item, i) => {
+      if(item.finished){
+        totalInByte += item.transferSize;
+        if(item.resourceType == "Document" || item.resourceType == "Script" || item.resourceType == "Other"){
+          totalInByteWithoutStatic += item.transferSize;
+        }
+      }
+    });
+
+    return {firstVisit: totalInByte / 1000, returningVisit: totalInByteWithoutStatic / 1000};
+  }
+
+  let calculateCarbonFootPrint = (pageSizeInKb, performanceScore) => {
+    let performancePunishment = 1 + (1-performanceScore/100);
+    console.log("performancePunishment", performancePunishment);
+    let firstVisitImpactKb = pageSizeInKb.firstVisit * performancePunishment;
+    let returningVisitImpactKb = pageSizeInKb.returningVisit * performancePunishment;
+
+
+    let firstVisitCarbon = firstVisitImpactKb * CARBON_PER_KB;
+    let returningVisitCarbon = returningVisitImpactKb * CARBON_PER_KB;
+
+    return {
+      sizeInKb: {firstVisit: pageSizeInKb.returningVisit, returningVisit: pageSizeInKb.returningVisit},
+      impactInKb: {firstVisit: firstVisitImpactKb, returningVisit: returningVisitImpactKb},
+      impactInCarbon: {firstVisit: firstVisitCarbon, returningVisit: returningVisitCarbon},
+    };
   }
 
   return (
@@ -34,6 +78,22 @@ export default function Home() {
           <input type="text" onChange={(e) => setUrl(e.target.value)}/>
           {!calculating && <div onClick={() => calculate()}>Calculate</div>}
           {calculating && <div>Calculating...</div>}
+          {footPrint.impactInCarbon &&
+            <div>
+              <p>Performance Score: {results.score} / 100</p>
+              <p></p>
+              <p>FirstVisit Load Kb: {footPrint.sizeInKb.firstVisit} kB</p>
+              <p>ReturningVisit Load Kb: {footPrint.sizeInKb.returningVisit} kB</p>
+              <p></p>
+
+              <p>FirstVisit Natural Impact Kb: {footPrint.impactInKb.firstVisit} kB</p>
+              <p>ReturningVisit Natural Impact Kb: {footPrint.impactInKb.returningVisit} kB</p>
+              <p></p>
+
+              <p>FirstVisit Carbon Footprint: {footPrint.impactInCarbon.firstVisit} g</p>
+              <p>ReturningVisit Carbon Footprint: {footPrint.impactInCarbon.returningVisit} g</p>
+            </div>
+          }
         </div>
       </div>
       <div className="content">
