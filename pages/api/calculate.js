643d605cdf6e6211de71b1ed675bd27092ec8e4d
Burak Tokak <iletisim@buraktokak.com>
2022-06-11 19:38:11 +0300

    Update calculate.js

--- pages/api/calculate.js
+++ pages/api/calculate.js
@@ -1,21 +1,24 @@
 const lighthouse = require('lighthouse-lambda-node12')
 
 const handler = async (req, res) => {
-  //let { url } = req.body
+  let { url } = req.body
 
   try {
 
     const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], onlyAudits: ['network-requests']};
-    const runnerResult = await lighthouse("https://carbonneutralwebsite.org/", options);
+    const runnerResult = await lighthouse(url, options);
 
     res.status(200).json({
-      url: runnerResult
+      url: runnerResult.results.lhr.finalUrl,
+      score: runnerResult.results.lhr.categories.performance.score * 100,
+      results: runnerResult.results.lhr.categories,
+      audits: runnerResult.results.lhr.audits
     })
 
     await chrome.kill();
 
   } catch (e) {
-    //res.status(500).json({ message: e.message })
+    res.status(500).json({ message: e.message })
   }
 }
 
