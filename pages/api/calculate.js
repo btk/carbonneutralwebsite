dd39915e7759dec35ac00318298ef9eafb92a15f
Burak Tokak <iletisim@buraktokak.com>
2022-10-16 22:17:29 +0300

    CREATE MY OWN LAMBDA F AND FIX THINGS HAHA

--- pages/api/calculate.js
+++ pages/api/calculate.js
@@ -1,21 +1,31 @@
-const lighthouse = require('./lib/index.js')
+//const lighthouse = require('./lib/index.js')
+async function post(url = '', address, mode) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: mode ? mode : 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: address // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
 
 const handler = async (req, res) => {
   let { url } = req.body
 
-  url = url || "http://buraktokak.com/"
+  url = url || "http://apple.com/";
 
   try {
-    console.log("started parsing", url);
-    const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], onlyAudits: ['network-requests']};
-    const runnerResult = await lighthouse(url, options);
-
-    res.status(200).json({
-      url: runnerResult.results.lhr.finalUrl,
-      score: runnerResult.results.lhr.categories.performance.score * 100,
-      results: runnerResult.results.lhr.categories,
-      audits: runnerResult.results.lhr.audits
-    })
+    let results = await post('https://crkt7fjddh2cy6z2xdzuuixwqy0fagsl.lambda-url.us-east-1.on.aws/', url);
+    res.status(200).json(results)
 
     //await chrome.kill();
 
