7a408b9ca32a2dbcca0b86a684224243d5d272cc
Burak Tokak <iletisim@buraktokak.com>
2022-11-18 20:46:24 +0300

    upgrade to next 13

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import { NextUIProvider } from '@nextui-org/react';
 
+import { Inter } from '@next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter()
+
 function MyApp({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
     <NextUIProvider>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily}, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+      `}</style>
       <Component {...pageProps} />
     </NextUIProvider>
   );
